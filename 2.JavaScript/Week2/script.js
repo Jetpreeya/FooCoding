69a49a835cc3864220c1c11b5019c0dac1466709
Jetpreeya Yawichai <84719740+Jetpreeya@users.noreply.github.com>
2021-10-17 17:08:33 +0200

    Add files via upload

---
+++ 2.JavaScript/Week2/script.js
@@ -0,0 +1,93 @@
+ //1
+ console.log("Hej Hej");
+  console.log("Sawadee");
+   console.log("NiHao");
+
+  //2
+ console.log("I'm awesome");
+
+ //3
+ console.log('x=90');
+    x = 32;
+ console.log(x);
+
+ //4
+ console.log('Soon');
+    y = 55;
+ console.log(y);
+ 
+ //5
+ z=7.25;
+ console.log(z);
+
+a=8;
+console.log(a);
+
+if(a>z){
+    console.log("true");
+}
+
+ //6
+const animals=["Dog","Cat","Dolphin","Bird"];
+console.log(animals);
+
+ //7
+let myString = "this is a test";
+myString.length = 5;
+console.log(myString);
+console.log(myString.length);
+
+ //8
+let foo = 9;
+console.log("Home:"+foo);
+console.log(typeof 89);
+console.log(typeof 'Dog');
+console.log(typeof true);
+console.log(typeof A);
+
+//8.6
+let m=99;
+let n="n";
+console.log(`let m: ${m} (${typeof m})`);
+console.log(`let n: ${n} (${typeof n})`);
+
+if (m == n) {
+  console.log(`Match! let m and let n are the same value.`);
+} else {
+  console.error(`No match: let m and let n are NOT same value.`);
+}
+
+ //9
+
+x=7;
+if (x= x % 3){
+   console.log(x);
+}
+
+y=5;
+if (y= y % 3){
+   console.log(y);
+}
+
+x=8;
+y=9;
+if(z= x % y){
+   console.log(z);
+}
+
+//10
+//10.1
+let menu = "BananaPie";
+const integiant = ["Banana", menu, "milk", "powder","sugar"];
+
+console.log(integiant);
+integiant[3]="WippingCream";
+
+//10.2,10.3
+e=6/0
+f=10/0
+if (e == f) {
+   console.log(`Match! let e and let f are the same value.`);
+ } else {
+   console.error(`No match: let e and let f are NOT same value.`);
+ }
