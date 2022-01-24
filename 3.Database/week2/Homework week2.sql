25934c1286dce738f9bae1f769c0b4ccd4bb791b
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-24 14:26:15 +0100

    Homework week2

---
+++ 3.Database/week2/Homework week2.sql
@@ -0,0 +1,6 @@
+--Part 1 --
+
+--1.What is the capital of country X ? (Accept X from user)
+= SELECT capital FROM country = X; 
+
+--2.List all the languages spoken in the region Y (Accept Y from user)
