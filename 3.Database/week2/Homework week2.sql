0db31694abf9e84c2d26a39a83fc9708a79a56b3
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-24 15:57:34 +0100

    homework week2

--- 3.Database/week2/Homework week2.sql
+++ 3.Database/week2/Homework week2.sql
@@ -1,6 +1,10 @@
 --Part 1 --
 
 --1.What is the capital of country X ? (Accept X from user)
-= SELECT capital FROM country = X; 
+
+mysql> PREPARE user FROM 'SELECT capital FROM country X';
+Query OK, 0 rows affected (0.00 sec)
+Statement prepared 
 
 --2.List all the languages spoken in the region Y (Accept Y from user)
+
