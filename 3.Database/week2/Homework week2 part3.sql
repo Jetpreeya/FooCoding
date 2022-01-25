82163f091bd5d9868f4c95f05067a72113f0006a
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-25 22:06:59 +0100

    part 3 finished 80%

--- 3.Database/week2/Homework week2 part3.sql
+++ 3.Database/week2/Homework week2 part3.sql
@@ -1,11 +1,11 @@
-
-delimiter //
-CREATE TRIGGER my_trigger BEFORE INSERT ON CountryLanguage 
+mysql> delimiter $$
+mysql> CREATE TRIGGER my_new BEFORE INSERT ON CountryLanguage 
 FOR EACH ROW BEGIN
-IF (Language >= 10) FROM CountryLanguage THEN INSERT INTO test VALUES ('WoW 10 languages');
+IF Language >= 10  THEN INSERT INTO test VALUES ('WoW 10 languages');
+ELSEIF Language < 10 THEN INSERT INTO test VALUES ('You can insert one more');
 END IF;
-END;//
-
-delimiter ;
-
+END;
+$$
+mysql> delimiter ;
+Query OK, 0 rows affected (0.02 sec)
 
