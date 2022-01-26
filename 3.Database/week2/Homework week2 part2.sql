a5755b3695b4d41c32117091a2778f1bb7d2a57d
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-26 14:20:53 +0100

    I finished homework week2 part 2

--- 3.Database/week2/Homework week2 part2.sql
+++ 3.Database/week2/Homework week2 part2.sql
@@ -25,17 +25,17 @@ mysql> DESCRIBE user;
 
 --Second list table 
 mysql> CREATE TABLE IF NOT EXISTS list 
-(listID INT(5) AUTO_INCREMENT PRIMARY KEY, 
+(listID INT(5) AUTO_INCREMENT , 
 listName VARCHAR(20) , 
 itemID int(5) NOT NULL references item(itemID), key list_item (listID, itemID));
 Query OK, 0 rows affected (0.05 sec)
 
-mysql> DESCRIBE list;
+mysql>  DESCRIBE list;
 +----------+-------------+------+-----+---------+----------------+
 | Field    | Type        | Null | Key | Default | Extra          |
 +----------+-------------+------+-----+---------+----------------+
-| listID   | int(5)      | NO   | PRI | NULL    | auto_increment |
-| listName | varchar(20) | YES  |     | NULL    |                |
+| listID   | int(5)      | NO   | MUL | NULL    | auto_increment |
+| listName | varchar(50) | YES  |     | NULL    |                |
 | itemID   | int(5)      | NO   |     | NULL    |                |
 +----------+-------------+------+-----+---------+----------------+
 3 rows in set (0.00 sec)
@@ -68,10 +68,10 @@ Query OK, 0 rows affected (0.06 sec)
 
 mysql> DESCRIBE tag;
 +-------------+-------------+------+-----+---------+----------------+
-| Field       | Type        | Null | Key | Default | Extra          |
+| Field       | Type         | Null | Key | Default | Extra          |
 +-------------+-------------+------+-----+---------+----------------+
-| tagID       | int(5)      | NO   | PRI | NULL    | auto_increment |
-| description | varchar(45) | YES  |     | NULL    |                |
+| tagID       | int(5)       | NO   | PRI | NULL    | auto_increment |
+| description | varchar(255) | YES  |     | NULL    |                |
 +-------------+-------------+------+-----+---------+----------------+
 2 rows in set (0.00 sec)
 
@@ -86,4 +86,88 @@ mysql> SHOW tables;
 | user              |
 +-------------------+
 4 rows in set (0.00 sec)
+
+--Insert data into table
+mysql> Insert into user (userID, FirstName, LastName, Email , ListID ) 
+VALUES 
+('23467', 'Susan', 'Kim', 'Susan@gmail.com','72346'),
+('55664', 'John', 'Alex', 'John.a@hotmail.com', '09876');
+
+mysql> SELECT userID, FirstName, LastName, Email , ListID FROM user;
++--------+-----------+----------+--------------------+--------+
+| userID | FirstName | LastName | Email              | ListID |
++--------+-----------+----------+--------------------+--------+
+|  23467 | Susan     | Kim      | Susan@gmail.com    |  72346 |
+|  55664 | John      | Alex     | John.a@hotmail.com |   9876 |
++--------+-----------+----------+--------------------+--------+
+2 rows in set (0.00 sec)
+
+--Each list has at least one item.
+
+mysql> Insert into list ( listID , listName , itemID  ) 
+VALUES 
+('9876', 'Send Email to company', '43567'), 
+('9876', 'Send Email to client','07654'),
+('72346','Buy food', '56456'),
+('72346','Buy food', '12367');
+
+mysql> SELECT listID , listName , itemID FROM list;
++--------+-----------------------+--------+
+| listID | listName              | itemID |
++--------+-----------------------+--------+
+|  72346 | Buy food              |  56456 |
+|  72346 | Buy food              |  12367 |
+|   9876 | Send Email to company |  43567 |
+|   9876 | Send Email to client  |   7654 |
++--------+-----------------------+--------+
+4 rows in set (0.00 sec)
+
+--Each item can be tagged.
+--Each item can be marked completed.
+mysql> Insert into item (itemID, tagID, marked, date) 
+VALUES
+('56456', '00001', 'Completed', '2022-01-25 11:15:00'),
+('12367', '00002', 'NOT','2022-01-26 11:20:001');
+
+mysql> Insert into item (itemID, tagID, marked) 
+VALUES
+('43567','00003','NOT'),
+('7654','00004','Completed');
+
+mysql> SELECT itemID, tagID, marked, date FROM item;
++--------+-------+-----------+---------------------+
+| itemID | tagID | marked    | date                |
++--------+-------+-----------+---------------------+
+|   7654 |     4 | Completed | NULL                |
+|  12367 |     2 | NOT       | 2022-01-26 11:20:01 |
+|  43567 |     3 | NOT       | NULL                |
+|  56456 |     1 | Completed | 2022-01-25 11:15:00 |
++--------+-------+-----------+---------------------+
+4 rows in set (0.00 sec)
+
+-- Insert into tag table
+mysql> Insert into tag (tagID, description) 
+VALUES 
+('1', 'Buy apple, milk and cheese before sunday'),
+('2', 'Buy agg, coffee, vine , pork before friday because my son birthday'), 
+('3','Send email to AB company'),
+('4', 'Send email information about the process ');
+
+
+mysql> SELECT tagID, description FROM tag;
++-------+--------------------------------------------------------------------+
+| tagID | description                                                        |
++-------+--------------------------------------------------------------------+
+|     1 | Buy apple, milk and cheese before sunday                           |
+|     2 | Buy agg, coffee, vine , pork before friday because my son birthday |
+|     3 | Send email to AB company                                           |
+|     4 | Send email information about the process                           |
++-------+--------------------------------------------------------------------+
+4 rows in set (0.00 sec)
+
+
+
+
+
+
 
