c5345cfa130b523d0961a51e53c969c94924393d
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-27 13:18:01 +0100

    Rewrite again

--- 3.Database/week2/Homework week2 part3.sql
+++ 3.Database/week2/Homework week2 part3.sql
@@ -9,3 +9,21 @@ $$
 mysql> delimiter ;
 Query OK, 0 rows affected (0.02 sec)
 
+--Rewrite by Johan--
+
+drop trigger if exists lang_count;
+delimiter $$
+create trigger lang_count
+before insert on countrylanguage
+for each row
+begin
+declare langcount int(2);
+set langcount = ( select count(1) from countrylanguage where countrycode = new.countrycode );
+if langcount  > 9
+then
+signal sqlstate '45000'
+  set message_text = 'You are trying to insert language 10 or above' ;
+end if;
+end;
+$$
+delimiter ;
