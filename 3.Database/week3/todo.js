181e8fc2c3b81a29ad6293521ac5c9428ea12219
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-28 16:06:14 +0100

    My todo list

---
+++ 3.Database/week3/todo.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var i1  = [];
+app.get("/", function(req, res) {
+    res.render("list",{newListItem:i1});
+});
+
+app.post("/", function(req,res){
+     i=req.body.n;
+    i1.push(i)
+    res.redirect("/");
+})
+
+app.listen(3000, function () {
+    console.log("App running at http://localhost:3000")
+});
+
