301cb9c0f0bfd71225a9cdf509b32fa679c9a32b
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-01-28 16:28:33 +0100

    todo list

--- 3.Database/week3/todo.js
+++ 3.Database/week3/todo.js
@@ -1,9 +1,18 @@
 var express = require("express");
+var mysql = require('mysql');
 var bodyParser = require("body-parser");
 var app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'me',
+    password : 'secret',
+    database : 'my_db'
+  });
+connection.connect();
 
 var i1  = [];
 app.get("/", function(req, res) {
